5a949d4f5a451bd4272cf66a21bc07013267619a
p.konischev <p.konischev@yadro.com>
2026-01-30 18:00:38 +0300

    update Direction and TxStatus

--- domain/src/main/kotlin/pablok/app/accounting/domain/Direction.kt
+++ domain/src/main/kotlin/pablok/app/accounting/domain/Direction.kt
@@ -1,4 +1,4 @@
 package pablok.app.accounting.domain
 
 /** Money flow direction relative to the account. */
-enum class Direction { IN, OUT }
+enum class Direction { IN, OUT, NEUTRAL, UNKNOWN }
--- ingestion-contracts/src/main/kotlin/pablok/app/accounting/ingestion/TxStatus.kt
+++ ingestion-contracts/src/main/kotlin/pablok/app/accounting/ingestion/TxStatus.kt
@@ -1,4 +1,9 @@
 package pablok.app.accounting.ingestion
 
 /** Source-level posting status as seen in a statement/source. */
-enum class TxStatus { POSTED, HOLD, UNKNOWN }
+enum class TxStatus {
+    POSTED,
+    HOLD,
+    REVERSAL,
+    UNKNOWN
+}
